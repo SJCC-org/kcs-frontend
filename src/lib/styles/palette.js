479ff6e9929bd8ebb1b60a3528e1df3992b309cc
Dev-jwJeong <rgn95652@gmail.com>
2022-07-09 15:32:00 +0900

    Add : gray,black 색 추가

--- src/lib/styles/palette.js
+++ src/lib/styles/palette.js
@@ -3,6 +3,16 @@ const palette = {
     //카카오 기본색
     '#FEE500',
   ],
+  gray: [
+    //헤더 테두리 색
+    '#eeeeee',
+    //input 테두리 색
+    '#CFD2CF',
+  ],
+  black: [
+    //버튼 글자 색
+    '#333',
+  ],
 };
 
 export default palette;
