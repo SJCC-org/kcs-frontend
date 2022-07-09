7fefaa8feda829ad6822e2597f796762225f56f4
Dev-jwJeong <rgn95652@gmail.com>
2022-07-09 15:33:52 +0900

    Add : 전체 공통 메인템플릿

---
+++ src/components/MainTemplete.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainTempleteBlock = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+`;
+
+const MainTemplete = () => {
+  return <MainTempleteBlock></MainTempleteBlock>;
+};
+
+export default MainTemplete;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainTemplete from '../components/MainTemplete';
+
+function MainPage() {
+  return (
+    <div>
+      <MainTemplete />
+    </div>
+  );
+}
+
+export default MainPage;
