5009344ae8922cd01b7a2350c57cc38123c49a4f
Dev-jwJeong <rgn95652@gmail.com>
2022-07-09 16:17:36 +0900

    Add : 홈페이지 아이콘

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import palette from '../lib/styles/palette';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import LoginForm from '../components/auth/LoginForm';
 
 const HeaderBlock = styled.div`
@@ -10,29 +10,31 @@ const HeaderBlock = styled.div`
   border-bottom: 1px solid ${palette.gray[0]};
   display: flex;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
   padding: 0 3rem;
 
-  button {
-    cursor: pointer;
-    outline: none;
-    margin-left: 1rem;
-    padding: 0.5rem 0.7rem;
-  }
-  #login {
-    background-color: ${palette.yellow[0]};
-    color: ${palette.black[0]};
-    font-weight: 400;
-    border: 2px solid ${palette.yellow[0]};
-    border-radius: 7px;
-  }
+  .buttonWrapper {
+    button {
+      cursor: pointer;
+      outline: none;
+      margin-left: 1rem;
+      padding: 0.5rem 0.7rem;
+    }
+    #login {
+      background-color: ${palette.yellow[0]};
+      color: ${palette.black[0]};
+      font-weight: 400;
+      border: 2px solid ${palette.yellow[0]};
+      border-radius: 7px;
+    }
 
-  #register {
-    background-color: white;
-    color: ${palette.black[0]};
-    font-weight: 400;
-    border: 2px solid ${palette.yellow[0]};
-    border-radius: 7px;
+    #register {
+      background-color: white;
+      color: ${palette.black[0]};
+      font-weight: 400;
+      border: 2px solid ${palette.yellow[0]};
+      border-radius: 7px;
+    }
   }
 `;
 
@@ -55,12 +57,17 @@ function Header() {
     <>
       {isOpen && <LoginForm onCloseLoginModal={onCloseLoginModal} />}
       <HeaderBlock>
-        <button id="login" onClick={onOpenLoginModal}>
-          로그인
-        </button>
-        <button id="register" onClick={onMoveRegister}>
-          회원가입
-        </button>
+        <h2>
+          <Link to="/">KCS</Link>
+        </h2>
+        <div className="buttonWrapper">
+          <button id="login" onClick={onOpenLoginModal}>
+            로그인
+          </button>
+          <button id="register" onClick={onMoveRegister}>
+            회원가입
+          </button>
+        </div>
       </HeaderBlock>
     </>
   );
