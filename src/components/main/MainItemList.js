a08d603c2d1512f632294ddfad703682d25d9e5e
Dev-jwJeong <rgn95652@gmail.com>
2022-07-09 18:00:51 +0900

    Add : 글쓰기 버튼 추가

--- src/components/main/MainItemList.js
+++ src/components/main/MainItemList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
 import MainItem from './MainItem';
 
 const MainItemListBlock = styled.div`
@@ -8,11 +9,30 @@ const MainItemListBlock = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 2rem;
+
+  .listHeader {
+    width: 100%;
+    text-align: right;
+    margin-bottom: 1rem;
+
+    button {
+      padding: 0.5rem 0.7rem;
+      background-color: ${palette.yellow[0]};
+      color: ${palette.black[0]};
+      font-weight: 500;
+      border: 2px solid ${palette.yellow[0]};
+      border-radius: 7px;
+      cursor: pointer;
+    }
+  }
 `;
 
 function MainItemList() {
   return (
     <MainItemListBlock>
+      <div className="listHeader">
+        <button>글쓰기</button>
+      </div>
       <MainItem />
       <MainItem />
       <MainItem />
