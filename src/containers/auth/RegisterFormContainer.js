a5afeefde2381e54a93b4a4d5dea024bf852b4f1
Dev-jwJeong <rgn95652@gmail.com>
2022-07-11 21:54:44 +0900

    Add : 회원가입 api 연동

--- src/containers/auth/RegisterFormContainer.js
+++ src/containers/auth/RegisterFormContainer.js
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import RegisterForm from '../../components/auth/RegisterForm';
-import { changeField, initailizeForm } from '../../modules/auth';
+import {
+  changeField,
+  duplicationUsernameFailure,
+  duplicationUsernameSuccess,
+  initailizeForm,
+  registerFailure,
+  registerSuccess,
+} from '../../modules/auth';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function RegisterFormContainer() {
-  const { form } = useSelector(({ auth }) => ({
+  const { form, userDuplicationRes, registerRes } = useSelector(({ auth }) => ({
     form: auth.register,
+    registerRes: auth.registerRes,
+    userDuplicationRes: auth.userDuplicationRes,
   }));
   const [error, setError] = useState(false);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const onCheckPasswordConfirm = () => {
     const { password, passwordConfirm } = form;
@@ -30,6 +42,19 @@ function RegisterFormContainer() {
     );
   };
 
+  async function getRegister() {
+    const { username, password, name, email } = form;
+    try {
+      const response = await axios.post(
+        'https://api.kcs.zooneon.dev/v1/members',
+        { username, password, name, email },
+      );
+      dispatch(registerSuccess(response.data.data));
+    } catch (e) {
+      dispatch(registerFailure(e));
+    }
+  }
+
   const onSubmit = (e) => {
     e.preventDefault();
     const { name, username, password, passwordConfirm, email } = form;
@@ -42,19 +67,50 @@ function RegisterFormContainer() {
       alert('비밀번호 확인을 해주세요');
       return;
     }
+
+    if (userDuplicationRes === null) {
+      alert('아이디 중복확인을 해주세요');
+      return;
+    }
+
+    getRegister();
+  };
+
+  async function duplicateUsername() {
+    const { username } = form;
+    try {
+      const response = await axios.get(
+        `https://api.kcs.zooneon.dev/v1/account/exists/${username}`,
+      );
+      dispatch(duplicationUsernameSuccess(response.data.data));
+    } catch (e) {
+      dispatch(duplicationUsernameFailure(e));
+    }
+  }
+
+  const onDuplicateUsername = () => {
+    duplicateUsername();
   };
 
   useEffect(() => {
     dispatch(initailizeForm('register'));
   }, [dispatch]);
 
+  useEffect(() => {
+    if (registerRes) {
+      navigate('/');
+    }
+  }, [registerRes, navigate]);
+
   return (
     <RegisterForm
       form={form}
       error={error}
+      userDuplicationRes={userDuplicationRes}
       onCheckPasswordConfirm={onCheckPasswordConfirm}
       onChange={onChange}
       onSubmit={onSubmit}
+      onDuplicateUsername={onDuplicateUsername}
     />
   );
 }
