2a29ac566e7c6aefda08a09560c2b3a65e0ff343
Dev-jwJeong <rgn95652@gmail.com>
2022-07-12 16:46:05 +0900

    Fix : Header UI

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -44,31 +44,12 @@ const HeaderBlock = styled.div`
     display: flex;
     align-items: center;
 
-    /* button {
-      cursor: pointer;
-      outline: none;
-      padding: 0.7rem 1.5rem;
-      font-size: 18px;
-
-      @media (max-width: 425px) {
-        padding: 0.5rem 0.7rem;
-        font-size: 16px;
-      }
-    } */
     #login {
-      /* background-color: ${palette.yellow[0]}; */
       color: ${palette.brown[0]};
       font-size: 18px;
       font-weight: bold;
       padding-left: 0.5rem;
       cursor: pointer;
-      /* border: 2px solid ${palette.yellow[0]}; */
-      /* border-radius: 7px; */
-
-      /* @media (max-width: 768px) {
-        display: none;
-      } */
-
       @media (max-width: 460px) {
         padding: 0.5rem 0.7rem;
         font-size: 16px;
@@ -112,9 +93,6 @@ function Header() {
           </div>
         </Link>
         <div className="buttonWrapper">
-          {/* <button id="login" onClick={() => setIsOpen(!isOpen)}>
-            로그인
-          </button> */}
           <div id="help" onClick={onCloseHelp}>
             문의사항
           </div>
