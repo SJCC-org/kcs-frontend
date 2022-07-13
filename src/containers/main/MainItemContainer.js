ee05442eaed86bfc6acf48a0041be29d32162850
Dev-jwJeong <rgn95652@gmail.com>
2022-07-13 23:31:16 +0900

    Add : 스터디 세부 정보 api 연동

--- src/containers/main/MainItemContainer.js
+++ src/containers/main/MainItemContainer.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import MainItem from '../../components/main/MainItem';
+import { getCookie } from '../../lib/cookie';
+import axios from 'axios';
+import { studyFailure, studySuccess } from '../../modules/study';
 
 function MainItemContainer() {
-  const { userRes } = useSelector(({ user }) => ({
-    userRes: user.userRes,
+  const { studyRes } = useSelector(({ study }) => ({
+    studyRes: study.studyRes,
   }));
-  return <MainItem userRes={userRes} />;
+  const dispatch = useDispatch();
+  const { studyId } = useParams();
+
+  useEffect(() => {
+    async function getStudy() {
+      const accessToken = getCookie('myAToken');
+      axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
+      try {
+        const response = await axios.get(
+          `https://api.kcs.zooneon.dev/v1/study/${studyId}`,
+        );
+        dispatch(studySuccess(response.data.data));
+      } catch (e) {
+        dispatch(studyFailure(e));
+      }
+    }
+    getStudy();
+  }, [dispatch, studyId]);
+
+  return <MainItem studyRes={studyRes} />;
 }
 
 export default MainItemContainer;
