6debfc128614a1b852f42703cb6d7fa9ba6f4d20
Dev-jwJeong <rgn95652@gmail.com>
2022-07-14 16:15:11 +0900

    Add : 스터디 페이지 조회 redux 연동

--- src/components/main/MainItem.js
+++ src/components/main/MainItem.js
@@ -4,40 +4,77 @@ import palette from '../../lib/styles/palette';
 
 const MainItemBlock = styled.div`
   width: 100%;
-  height: 100px;
   background-color: white;
   border-radius: 7px;
   margin-bottom: 1rem;
   box-shadow: 4px 12px 30px 6px rgb(0 0 0 / 9%);
   cursor: pointer;
   padding: 1rem;
-
-  h3 {
-    margin: 0;
-  }
-
-  .studyStatus {
-    width: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 7px;
-    font-size: 14px;
-    padding: 0.3rem;
-    font-weight: bold;
-    margin-bottom: 1rem;
-    background-color: ${palette.yellow[0]};
-  }
 `;
 
-function MainItem({ userRes, studyRes }) {
+const StudyItemHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+
+  .studyInfo {
+    display: flex;
+    align-items: center;
+
+    .studyStatus {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 7px;
+      font-size: 14px;
+      padding: 0.5rem;
+      background-color: ${palette.yellow[0]};
+      margin-right: 0.5rem;
+    }
+  }
+
+  .studyDate {
+    font-size: 15px;
+    color: ${palette.gray[2]};
+  }
+`;
+const StudyItemTitle = styled.div`
+  width: 100%;
+  h3 {
+    margin: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+const StudyItemNum = styled.div`
+  width: 100%;
+  text-align: right;
+`;
+function MainItem({ data, onMoveStudyInfo }) {
   return (
-    studyRes && (
-      <MainItemBlock>
-        <div className="studyStatus">모집중</div>
-        <h3>스터디를 합시다</h3>
-      </MainItemBlock>
-    )
+    <MainItemBlock onClick={() => onMoveStudyInfo(data.id)}>
+      <StudyItemHeader>
+        <div className="studyInfo">
+          {data.recruitCompleted ? (
+            <div className="studyStatus">모집완료</div>
+          ) : (
+            <div className="studyStatus">모집중</div>
+          )}
+          <div className="studyStatus">{data.studyCategory}</div>
+        </div>
+        <div className="studyDate">{data.createdDate.substr(0, 10)}</div>
+      </StudyItemHeader>
+      <StudyItemTitle>
+        <h3>{data.title}</h3>
+      </StudyItemTitle>
+      <StudyItemNum>
+        <span>{data.curNum}</span>
+        <span>/</span>
+        <span>{data.maxNum}</span>
+      </StudyItemNum>
+    </MainItemBlock>
   );
 }
 
