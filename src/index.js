4cb4e33d2c218d2cfadfd1cd27689662545a41b1
Dev-jwJeong <rgn95652@gmail.com>
2023-02-14 17:41:25 +0900

    style : globalStyle 적용

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import rootReducer from './modules';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { Provider } from 'react-redux';
-import { HelmetProvider } from 'react-helmet-async';
-import { getCookie, removeCookie } from './lib/cookie';
-import { userFailure, userSuccess } from './modules/user';
-import axios from 'axios';
-import jwt from 'jwt-decode';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import { HelmetProvider } from "react-helmet-async";
+import { getCookie, removeCookie } from "./lib/cookie";
+import { userFailure, userSuccess } from "./modules/user";
+import axios from "axios";
+import jwt from "jwt-decode";
+import { GlobalStyle } from "./lib/styles/globalStyle";
 
 const store = createStore(rootReducer, composeWithDevTools());
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 async function loadUser() {
-  const accessToken = getCookie('myAToken');
-  const refreshToken = getCookie('myRToken');
+  const accessToken = getCookie("myAToken");
+  const refreshToken = getCookie("myRToken");
   if (!refreshToken) {
     return;
   }
@@ -27,13 +26,13 @@ async function loadUser() {
   const { exp } = jwt(refreshToken);
 
   if (Date.now() >= exp * 1000) {
-    removeCookie('myAToken');
-    removeCookie('myRToken');
+    removeCookie("myAToken");
+    removeCookie("myRToken");
   } else if (Date.now() < jwt(accessToken).exp * 1000) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
     try {
       const response = await axios.get(
-        'https://api.kcs.zooneon.dev/v1/members',
+        "https://api.kcs.zooneon.dev/v1/members"
       );
       store.dispatch(userSuccess(response.data.data));
     } catch (e) {
@@ -45,15 +44,11 @@ loadUser();
 
 root.render(
   <Provider store={store}>
+    <GlobalStyle />
     <BrowserRouter>
       <HelmetProvider>
         <App />
       </HelmetProvider>
     </BrowserRouter>
-  </Provider>,
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
