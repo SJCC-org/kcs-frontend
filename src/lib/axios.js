83921184ac1fdc951a2109f43ebeacb4261fb795
Dev-jwJeong <rgn95652@gmail.com>
2023-02-15 15:33:17 +0900

    feat : swr fetcher, axios interceptor 설정

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
+const ACCESS_TOKEN = localStorage.getItem("accessToken");
+
 export const client = axios.create({
   baseURL: `${process.env.REACT_APP_BASE_URL}`,
   headers: {
     "Content-type": "application/json",
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
   },
 });
+
+client.interceptors.request.use(function (config) {
+  config.headers.common["Authorization"] = localStorage.getItem("accessToken");
+  return config;
+});
+
+export const kakaoGetFetcher = (url) =>
+  client.get(url).then((res) => res.data.data);
