e28754a2213c8ddad0f7e677c1926988f6e2e62a
Dev-jwJeong <rgn95652@gmail.com>
2023-02-17 16:06:42 +0900

    feat : 카테고리 목록 상수

---
+++ src/constants/category.js
@@ -0,0 +1,7 @@
+export const CATEGORY_LIST = [
+  { id: 1, ko: "알고리즘", en: "ALGORITHM" },
+  { id: 2, ko: "자격증", en: "CERTIFICATE" },
+  { id: 3, ko: "프로젝트", en: "PROJECT" },
+  { id: 4, ko: "CS", en: "CS" },
+  { id: 5, ko: "기타", en: "ETC" },
+];
